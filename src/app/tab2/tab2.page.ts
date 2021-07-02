41eb3f603475d6310b34b5dffa6dfdd456951e9c
christiankozalla <christian.kozalla@gmail.com>
2021-07-02 11:35:41 +0200

    Pulls posts from backend

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  posts: Observable<any>;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.posts = this.http.get('http://localhost:4000/posts');
+    this.posts.subscribe((data) => {
+      console.log('puroviva posts', data);
+    });
+  }
 }
