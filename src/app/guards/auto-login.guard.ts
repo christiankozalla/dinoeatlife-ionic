2ef3d9cda44640d8ddfb827aa8169770f62a8666
christiankozalla <christian.kozalla@gmail.com>
2021-07-19 13:28:55 +0200

    Updates auto-login guard

--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { CanLoad, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { filter, map, take } from 'rxjs/operators';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutoLoginGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+
+  canLoad(): Observable<boolean> {
+    return this.authService.isAuthenticated.pipe(
+      filter((val) => val !== null),
+      take(1),
+      map((isAuthenticated) => {
+        if (isAuthenticated) {
+          console.log('Found exsiting token, auto login!');
+          // Redirect to home
+          this.router.navigateByUrl('/home', { replaceUrl: true });
+          return false;
+        } else {
+          // Allow access to login page
+          return true;
+        }
+      })
+    );
   }
 }
